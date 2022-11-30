2900af3f1b4b14097049f68c7885bc2968614f60
chikien111 <38467294+chikien111@users.noreply.github.com>
2022-11-30 09:19:01 +0700

    update home-page-noel & thanh-pho-3c

--- home-page-2022-noel/scripts/an.js
+++ home-page-2022-noel/scripts/an.js
@@ -32,6 +32,7 @@ $(".share-khoanh-khac .info-gift .item ").hover(function(){
 });
 
 
+
 var hoverandclick=1;
  $(".blk-giao-bia-nhanh .inner").hover(function(){
    
@@ -313,7 +314,7 @@ $(".click-rotate").click(function(){
 
       $(".slider-url .inner-slider-url .item").removeClass("active");
       $(".slider-url .inner-slider-url .data-hash-clear").addClass("active");
-      deg -=50;
+      deg -=100;
       $(".image-rotate .img").css("transform","rotate("+ deg +"deg)");
     }, 4000);
     setTimeout(function(){
@@ -322,7 +323,7 @@ $(".click-rotate").click(function(){
       $(".slider-url .inner-slider-url .item").removeClass("active");
       $(".slider-url .inner-slider-url .data-hash-creamy").addClass("active");
       // $(".image-rotate .img").css("transform","rotate(-270deg)");
-      deg -=77;
+      deg -=154;
       $(".image-rotate .img").css("transform","rotate("+ deg +"deg)");
     }, 7000);
      setTimeout(function(){
@@ -336,7 +337,7 @@ $(".click-rotate").click(function(){
       $(".blk-whell").removeClass("hidden");
       $(".click-rotate").removeClass("disabled-click");
       // $(".image-rotate .img").css("transform","rotate(0deg)");
-      deg -=53;
+      deg -=106;
       $(".image-rotate .img").css("transform","rotate("+ deg +"deg)");
     }, 10000);
     
@@ -389,3 +390,115 @@ $(document).ready(function(){
 //     // smoke_animation();
 // })
 
+const CONSTANTS = {
+  VX_MAX: 2,
+  VY_MAX: 5,
+  VY_ACCELERATE: 0.005,
+};
+class SnowFlake {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.radius = 0;
+    this.alpha = 0;
+    this.opacity = 1;
+    this.reset();
+    this.disappear();
+  }
+  
+  disappear() {
+    this.opacity = this.opacity - 0.025;
+  }
+
+  reset() {
+    this.opacity = 1;
+    this.radius = this.randBetween(1, 3);
+    this.alpha = this.randBetween(0.3, 0.9);
+    this.x = this.randBetween(0, window.innerWidth);
+    this.y = this.randBetween(-window.innerHeight, -this.radius);
+    this.vx = this.randBetween(-CONSTANTS.VX_MAX, CONSTANTS.VX_MAX);
+    this.vy = this.randBetween(0, 1);
+  }
+
+  update() {
+    if(this.vy < CONSTANTS.VY_MAX) {
+      this.vy += CONSTANTS.VY_ACCELERATE;
+    }
+    if(this.vx < CONSTANTS.VX_MAX) {
+      this.vx += this.randBetween(0, 0.3);
+    }
+    if(this.vx > -CONSTANTS.VX_MAX) {
+      this.vx -= this.randBetween(0, 0.3);
+    }
+    this.x += this.vx;
+    this.y += this.vy;
+    
+    if(this.y + this.radius > window.innerHeight - 200) {
+      this.disappear();
+    }
+    
+    if(this.y + this.radius > window.innerHeight) {
+      this.reset();
+    }
+    
+    
+  }
+
+  randBetween(min, max) {
+    return min + Math.random() * (max - min);
+  }
+}
+class Snow {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.querySelector('#ani-snow').appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+    this.onResize();
+    window.addEventListener('resize', () => { this.onResize() });
+
+
+    this.updateBound = this.update.bind(this);
+    requestAnimationFrame(this.updateBound);
+
+    this.createSnowFlakes();
+  }
+
+  onResize() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+  }
+
+  createSnowFlakes() {
+    this.snowFlakesNum = window.innerHeight * window.innerWidth / 3000;
+    this.snowFlakes = [];
+    for (let i = 0; i < this.snowFlakesNum; i++) {
+      this.snowFlakes.push(new SnowFlake());
+    }
+  }
+
+  update() {
+    this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    this.snowFlakes.forEach(snowFlake => {
+      snowFlake.update();
+
+      // draw snow flake
+      this.ctx.save();
+      this.ctx.fillStyle = "rgba(255, 255, 255, "+ snowFlake.opacity +")";
+      this.ctx.beginPath();
+      this.ctx.arc(snowFlake.x, snowFlake.y, snowFlake.radius, 0, Math.PI * 2);
+      this.ctx.closePath();
+      this.ctx.globalAlpha = snowFlake.alpha;
+      this.ctx.fill();
+      this.ctx.restore();
+    });
+
+    requestAnimationFrame(this.updateBound);
+  }
+}
+
+// let it snow
+new Snow();
